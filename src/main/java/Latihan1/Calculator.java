2565898326ab3bcf77ecc7f618357ebd4fac126f
Ferdy Rizal Mahendra Putra <nakalrizal472@gmail.com>
2023-11-14 20:29:32 +0700

    Mengisi Folder

--- src/main/java/Latihan1/Calculator.java
+++ src/main/java/Latihan1/Calculator.java
@@ -1,2 +1,85 @@
-package Latihan1;public class Calculator {
+package Latihan1;
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("=== Calculator ===");
+            System.out.println("1. Akar Kuadrat");
+            System.out.println("2. Pangkat");
+            System.out.println("3. Logaritma");
+            System.out.println("4. Faktorial");
+            System.out.println("5. Keluar");
+            System.out.print("Pilih operasi (1/2/3/4/5): ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    akarKuadrat();
+                    break;
+                case 2:
+                    pangkat();
+                    break;
+                case 3:
+                    logaritma();
+                    break;
+                case 4:
+                    faktorial();
+                    break;
+                case 5:
+                    System.out.println("Terima kasih! Keluar dari program.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih kembali.");
+            }
+        }
+    }
+
+    private static void akarKuadrat() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        double angka = scanner.nextDouble();
+
+        if (angka < 0) {
+            System.out.println("Angka harus non-negatif untuk akar kuadrat.");
+        } else {
+            double hasil = Math.sqrt(angka);
+            System.out.println("Hasil akar kuadrat dari " + angka + " adalah " + hasil);
+        }
+    }
+
+    private static void pangkat() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        double angka = scanner.nextDouble();
+        System.out.print("Masukkan pangkat: ");
+        double pangkat = scanner.nextDouble();
+
+        double hasil = Math.pow(angka, pangkat);
+        System.out.println("Hasil " + angka + " pangkat " + pangkat + " adalah " + hasil);
+    }
+
+    private static void logaritma() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        double angka = scanner.nextDouble();
+
+        double hasil = Math.log10(angka);
+        System.out.println("Hasil logaritma basis 10 dari " + angka + " adalah " + hasil);
+    }
+
+    private static void faktorial() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = scanner.nextInt();
+
+        int hasil = 1;
+        for (int i = 1; i <= angka; i++) {
+            hasil *= i;
+        }
+        System.out.println("Hasil faktorial dari " + angka + " adalah " + hasil);
+    }
 }
--- src/main/java/Latihan1/TextAnalyzer.java
+++ src/main/java/Latihan1/TextAnalyzer.java
@@ -1,2 +1,70 @@
-package Latihan1;public class TextAnalyzer {
+package Latihan1;
+import java.util.Scanner;
+public class TextAnalyzer {
+    private static String teks = "";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("=== MENU TEKS ANALYZER ===");
+            System.out.println("1. Masukkan Teks");
+            System.out.println("2. Hitung Jumlah Karakter");
+            System.out.println("3. Hitung Jumlah Kata");
+            System.out.println("4. Cari Kata dalam Teks");
+            System.out.println("5. Keluar");
+            System.out.print("Pilihan Anda: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    masukkanTeks(scanner);
+                    break;
+                case 2:
+                    hitungJumlahKarakter();
+                    break;
+                case 3:
+                    hitungJumlahKata();
+                    break;
+                case 4:
+                    cariKata(scanner);
+                    break;
+                case 5:
+                    System.out.println("Terima kasih! Keluar dari program.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih kembali.");
+            }
+        }
+    }
+
+    private static void masukkanTeks(Scanner scanner) {
+        System.out.print("Masukkan teks: ");
+        scanner.nextLine(); // membersihkan buffer
+        teks = scanner.nextLine();
+        System.out.println("Teks telah dimasukkan!\n");
+    }
+
+    private static void hitungJumlahKarakter() {
+        System.out.println("Jumlah karakter dalam teks: " + teks.length() + "\n");
+    }
+
+    private static void hitungJumlahKata() {
+        String[] kataArray = teks.split("\\s+");
+        System.out.println("Jumlah kata dalam teks: " + kataArray.length + "\n");
+    }
+
+    private static void cariKata(Scanner scanner) {
+        System.out.print("Masukkan kata yang ingin dicari: ");
+        String kataCari = scanner.next();
+        boolean kataDitemukan = teks.contains(kataCari);
+
+        if (kataDitemukan) {
+            System.out.println("Kata '" + kataCari + "' ditemukan dalam teks.\n");
+        } else {
+            System.out.println("Kata '" + kataCari + "' tidak ditemukan dalam teks.\n");
+        }
+    }
 }
